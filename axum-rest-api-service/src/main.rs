27baa5d03b7663d25cdcb2042093609ab3865430
Jose Rodriguez <4713082+josephrodriguez@users.noreply.github.com>
2025-03-30 12:41:40 +0300

    Configure and run Axum serve

--- axum-rest-api-service/src/main.rs
+++ axum-rest-api-service/src/main.rs
@@ -1,8 +1,8 @@
-use std::env;
-use std::error::Error;
 use axum::Router;
 use env_logger::{Builder, WriteStyle};
-use log::{info, LevelFilter};
+use log::{LevelFilter, info};
+use std::env;
+use std::error::Error;
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -12,10 +12,21 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .write_style(WriteStyle::Always)
         .init();
 
-    let port = env::var("RUST_STARTER_KIT_AXUM_RESTAPI_PORT").unwrap_or_else(|_| "3000".to_string());
+    info!("Starting up...");
+
+    let port =
+        env::var("RUST_STARTER_KIT_AXUM_RESTAPI_PORT").unwrap_or_else(|_| "3000".to_string());
     let address = format!("0.0.0.0:{}", port);
 
-    info!("Hello, world from Rust with Axum!");
+    let app = Router::new();
+    let listener = TcpListener::bind(address).await.unwrap();
+
+    info!(
+        "Server running at http://{}",
+        listener.local_addr().unwrap()
+    );
+
+    axum::serve(listener, app).await.unwrap();
 
     Ok(())
 }
