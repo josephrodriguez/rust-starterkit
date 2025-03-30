fc3ff704b22bd98f961ae4f0edc1f00c41f80d5d
Jose Rodriguez <4713082+josephrodriguez@users.noreply.github.com>
2025-03-30 14:47:59 +0300

    Configure dependabot for cargo

--- axum-rest-api-service/src/handlers/get_movies_handler.rs
+++ axum-rest-api-service/src/handlers/get_movies_handler.rs
@@ -1,10 +1,13 @@
 use axum::http::StatusCode;
 use axum::Json;
 use axum::response::IntoResponse;
+use log::info;
 use tokio::fs;
 use crate::models::Movie;
 
 pub(crate) async fn handle_get_movies() -> impl IntoResponse {
+    info!("Handle GET /movies endpoint request");
+
     let movies_json = match fs::read_to_string("../assets/movies.json").await {
         Ok(data) => data,
         Err(e) => return (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
