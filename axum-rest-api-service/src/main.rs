d5d56fb2e5128bec66b39d3e0a7c90b91becbc66
Jose Rodriguez <4713082+josephrodriguez@users.noreply.github.com>
2025-03-30 12:34:51 +0300

    Read port variable with fallback option from environment variable

--- axum-rest-api-service/src/main.rs
+++ axum-rest-api-service/src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use env_logger::{Builder, WriteStyle};
 use log::{info, LevelFilter};
 
@@ -6,6 +7,9 @@ fn main() {
         .filter_level(LevelFilter::Debug)
         .write_style(WriteStyle::Always)
         .init();
+
+    let port = env::var("RUST_STARTER_KIT_AXUM_RESTAPI_PORT").unwrap_or_else(|_| "3000".to_string());
+    let address = format!("0.0.0.0:{}", port);
 
     info!("Hello, world from Rust with Axum!");
 }
